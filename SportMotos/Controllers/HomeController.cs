8952b0f2c386960deb3f98fb2584e9758ef84765
André Gomes <a22618@esenviseu.net>
2025-02-06 20:45:38 +0000

    Ajustes no login, ver dashboard, adicionar DB

--- SportMotos/Controllers/HomeController.cs
+++ SportMotos/Controllers/HomeController.cs
@@ -33,6 +33,11 @@ namespace SportMotos.Controllers
             return View();
         }
 
+        public IActionResult DashBoard()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
