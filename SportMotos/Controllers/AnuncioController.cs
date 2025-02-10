c08365ad7e60d9320573b1f1fbf9932e787b6551
DeepDish36 <andredeepgomes@gmail.com>
2025-02-10 17:17:07 +0000

    Criação dos cards para os anúncios, fazer alguns ajustes

---
+++ SportMotos/Controllers/AnuncioController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SportMotos.Models;
+
+namespace SportMotos.Controllers
+{
+    public class AnuncioController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public AnuncioController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Anuncio(string tipo)
+        {
+            if (tipo == "motos")
+            {
+                // Busca anúncios de motos
+                var anunciosMotos = await _context.AnuncioMotos
+                    .Where(a => a.ApagadoEm == null) // Apenas anúncios ativos
+                    .ToListAsync();
+                ViewBag.TipoAnuncio = "motos";
+                return View(anunciosMotos);
+            }
+            else if (tipo == "pecas")
+            {
+                // Busca anúncios de peças
+                var anunciosPecas = await _context.AnuncioPecas
+                    .Where(a => a.ApagadoEm == null) // Apenas anúncios ativos
+                    .Include(a => a.IdPecaNavigation) // Inclui a tabela Peca para obter o Stock
+                    .ToListAsync();
+
+                ViewBag.TipoAnuncio = "pecas";
+                // Passa os dados para a View
+                return View(anunciosPecas);
+            }
+
+            // Se o tipo não for especificado, redireciona para uma página de erro ou exibe todos os anúncios
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
